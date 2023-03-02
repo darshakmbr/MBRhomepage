c3a5e01c246dbdd99d67716b5ee403d162a142fe
darshakmbr <darshak@mybespokeroom.com>
2023-03-02 22:06:22 +0000

    Locofy commit

--- index.js
+++
@@ -1,9 +0,0 @@
-$(".homepage-desk-top-inner").hide();
-$(".homepage-desk-top-child1").hide();
-$(".close-button-icon").on("click",function() {
-    $(".homepage-desk-top-inner").hide();
-});
-
-$(".rectangle-parent10").click(function() {
-    $(".homepage-desk-top-inner").show();
-})
