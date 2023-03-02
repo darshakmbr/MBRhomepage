41326202bed63ef92bb3c1af05a25199d83de268
Darshak Shah <darshak@applieddigital.co.uk>
2023-03-03 02:18:34 +0530

    homepage desktop changes

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ $(".homepage-desk-top-child1").hide();
 $(".close-button-icon").on("click",function() {
     $(".homepage-desk-top-inner").hide();
 });
+
+$(".rectangle-parent10").click(function() {
+    $(".homepage-desk-top-inner").show();
+})
